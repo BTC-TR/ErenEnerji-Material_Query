1015d965951cf6b0078152e5a5f778639fdadc06
gorkem-aydin <gorkemaydin@gmx.com>
2024-05-24 13:53:03 +0000

    test

--- webapp/controller/Table.controller.js
+++ webapp/controller/Table.controller.js
@@ -22,7 +22,7 @@ sap.ui.define([
         onInit: function () {
             this._mViewSettingsDialogs = {};
 
-
+//
             this.mGroupFunctions = {
 
                 Lgpla: function (oContext) {
